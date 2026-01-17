bb5f5bc1e471274893a5173ddd908d02dad9cfc3
Gyanni <lghlee36@gmail.com>
2026-01-17 21:23:49 +0900

    vpc

--- terraform/vpc/abcd_apnortheast2/backend.tf
+++ terraform/vpc/abcd_apnortheast2/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.0.0"
 
   backend "s3" {
-    bucket         = "zerone-id-apnortheast2-tfstate"
+    bucket         = "gyanni-id-apnortheast2-tfstate"
     key            = "provisioning/terraform/vpc/tmcd_apnortheast2/terraform.tfstate"
     region         = "ap-northeast-2"
     encrypt        = true

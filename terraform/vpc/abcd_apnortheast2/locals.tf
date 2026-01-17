3b3020dc909c7c23a553e4191380698f1339e15d
Gyanni <lghlee36@gmail.com>
2026-01-17 22:19:34 +0900

    vpc

--- terraform/vpc/abcd_apnortheast2/locals.tf
+++ terraform/vpc/abcd_apnortheast2/locals.tf
@@ -9,37 +9,45 @@ locals {
   vpc_name         = replace("${var.product}${var.env_suffix}_${var.aws_region}", "-", "")
   shard_id         = var.shard_id
   eks_cluster_name = format("%s%s%s-%s", var.product, var.env_suffix, var.aws_short_region, random_string.suffix.result)
-  vpc_tags = {
-    "kubernetes.io/cluster/${local.eks_cluster_name}" : "shared",
-    "Name" : "vpc-${local.vpc_name}"
-  }
-  public_subnets_tags = {
-    "kubernetes.io/cluster/${local.eks_cluster_name}" : "shared"
-    "kubernetes.io/role/elb" : 1
-  }
-  private_subnets_tags = {
-    Network = "Private"
-    "kubernetes.io/cluster/${local.eks_cluster_name}" : "shared"
-    "kubernetes.io/role/internal-elb" : 1
-  }
-  public_subnets_tags_flat = distinct(flatten([
-    for id in aws_subnet.public.*.id : [
-      for key, value in local.public_subnets_tags : {
-        id    = id
-        key   = key
-        value = value
+
+  # Static keys for vpc_tags to satisfy for_each requirement
+  vpc_tags_list = [
+    { key = "kubernetes.io/cluster/${local.eks_cluster_name}", value = "shared" },
+    { key = "Name", value = "vpc-${local.vpc_name}" }
+  ]
+
+  # Static keys for subnet tags
+  public_subnets_tags_list = [
+    { key = "kubernetes.io/cluster/${local.eks_cluster_name}", value = "shared" },
+    { key = "kubernetes.io/role/elb", value = "1" }
+  ]
+  private_subnets_tags_list = [
+    { key = "Network", value = "Private" },
+    { key = "kubernetes.io/cluster/${local.eks_cluster_name}", value = "shared" },
+    { key = "kubernetes.io/role/internal-elb", value = "1" }
+  ]
+
+  # Flatten with static index-based keys
+  public_subnets_tags_flat = flatten([
+    for idx in range(length(var.availability_zones)) : [
+      for tag_idx, tag in local.public_subnets_tags_list : {
+        static_key  = "${idx}_${tag_idx}"
+        subnet_idx  = idx
+        key         = tag.key
+        value       = tag.value
       }
     ]
-  ]))
-  private_subnets_tags_flat = distinct(flatten([
-    for id in aws_subnet.private.*.id : [
-      for key, value in local.private_subnets_tags : {
-        id    = id
-        key   = key
-        value = value
+  ])
+  private_subnets_tags_flat = flatten([
+    for idx in range(length(var.availability_zones)) : [
+      for tag_idx, tag in local.private_subnets_tags_list : {
+        static_key  = "${idx}_${tag_idx}"
+        subnet_idx  = idx
+        key         = tag.key
+        value       = tag.value
       }
     ]
-  ]))
+  ])
 
   private_subnet_peerings = flatten([
     for pair in setproduct(aws_route_table.private.*.id, var.peering_requests) : {
